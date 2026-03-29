2f5614d9dc9ba8c02ae3275621eb297a54506273
hodewdew-creator <moyadais@gmail.com>
2026-03-29 13:12:26 +0900

    Add RootLayout component with metadata

---
+++ layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Monitoring Tool",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
